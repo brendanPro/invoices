b3b40fac6132b769e66fb3323881c49a08230bc1
brendiche <brendan.gouin92@gmail.com>
2025-10-27 17:37:29 +0100

    fix: build env var

--- build.ts
+++ build.ts
@@ -150,6 +150,7 @@ const result = await build({
   sourcemap: "linked",
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
+    "process.env.BUN_PUBLIC_BACKEND_URL": JSON.stringify(process.env.BUN_PUBLIC_BACKEND_URL || "http://localhost:9999"),
   },
   ...cliConfig, // Merge in any CLI-provided options
 });
